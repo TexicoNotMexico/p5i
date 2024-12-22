2eb2333b749fe878ba995abe9b2c9eee40093998
TexicoNotMexico <haruhitexico0118@gmail.com>
2024-12-22 21:15:14 +0900

    copyright clear

---
+++ README.md
@@ -0,0 +1,9 @@
+# p5i
+
+## 利用著作物
+
+-   [Noto Sans JP](https://fonts.google.com/noto/specimen/Noto+Sans+JP) (`public\NotoSansJP-Bold.otf`, `NotoSansJP-Medium.otf`)
+    -   SIL Open Font License
+
+-   [いはないお約束](https://www.nicovideo.jp/watch/sm43193515) (`public\ihanaifull.wav`)
+    -   by ァネイロ
--- src/fonts.ts
+++ src/fonts.ts
@@ -6,8 +6,8 @@ export let lyricFont: p5.Font;
 export let rubyFont: p5.Font;
 
 export const fontPreload = () => {
-    lyricFont = p.loadFont("NotoSansJP-Medium.otf"); // p.loadFont("TenMinchoAntique-Medium.otf");
-    rubyFont = p.loadFont("NotoSerifJP-Regular.otf"); // p.loadFont("TenMinchoAntique-Light.otf");
+    lyricFont = p.loadFont("NotoSansJP-Medium.ttf"); // p.loadFont("TenMinchoAntique-Medium.otf");
+    rubyFont = p.loadFont("NotoSerifJP-Regular.ttf"); // p.loadFont("TenMinchoAntique-Light.otf");
 };
 
 export const setLyricFont = () => {
